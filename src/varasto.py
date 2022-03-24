aa131df15736f40ccdb3ddf4c3adb6861951b212
Emma Piittala <emma.piittala@gmail.com>
2022-03-24 19:38:42 +0200

    tehtävän teko

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
+        paras = max(0,tilavuus)
+        self.tilavuus = paras
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -27,11 +23,10 @@ class Varasto:
             self.saldo = self.saldo + maara
         else:
             self.saldo = self.tilavuus
-
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +35,5 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
